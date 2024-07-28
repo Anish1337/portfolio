a5cd871a05357bef90048e594fbfc3d77b79dca1
Anish Tiwari <anishtiwari425@gmail.com>
2024-07-28 10:54:03 -0400

    formatting

--- portfolio/main/static/js/home.js
+++ portfolio/main/static/js/home.js
@@ -1,3 +1,4 @@
+// todo
 document.addEventListener("DOMContentLoaded", function () {
     const nameSearch = document.getElementById("name-search")
     const tags = document.querySelectorAll(".tag")
