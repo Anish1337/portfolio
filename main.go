efee58c42b19f04f953abb08f5ee9877b2948a61
Anish Tiwari <anishtiwari425@gmail.com>
2024-12-29 18:34:12 -0500

    completed template files for ecah page

--- main.go
+++ main.go
@@ -1,37 +1,59 @@
-package main
+package main // Declare the main package
 
 import (
-    "fmt"
-    "html/template"
-    "log"
-	"net/http"
+	"fmt"           // Import the fmt package for formatted I/O
+	"html/template" // Import the html/template package for rendering HTML templates
+	"log"           // Import the log package for logging errors
+	"net/http"      // Import the net/http package for HTTP server and client
 )
 
 func main() {
-    // route handler
 	http.HandleFunc("/", home)
-    http.HandleFunc("/projects", projects)
-    http.HandleFunc("/skills", skills)
-    http.HandleFunc("/about", about)
-    http.HandleFunc("/contact", contact)
+	http.HandleFunc("/projects", projects)
+	http.HandleFunc("/skills", skills)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/contact", contact)
 
-    // start server
-    htpp.ListenAndServer(":8008", nil)
+	fmt.Println("🚀 Server is running on http://localhost:8080")
+
+    // Start server (port 8080)
+	err := http.ListenAndServe(":8080", nil)
+   
+    // Check + log errors
+	if err != nil { 
+		log.Fatal(err) 
+	}
 }
 
-// Global Scope
+// page functions
 
-// home function handles request to homepage
 func home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.html")
+	renderTemplate(w, "home.html") 
+}
 
-    
-}  
+func projects(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "projects.html") 
+}
 
-func rendedrTempalte(w http.ResponseWrioter, tmpl string){
-    t, err := template.ParseFiles("tempaltes/" + tmpl)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError) 
-    }
-    t.Execute(w, nil)
+func skills(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "skills.html") 
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.html") 
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "contact.html") 
+}
+
+// renderTemplate function
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	t, err := template.ParseFiles("templates/" + tmpl) 
+
+    // error check
+	if err != nil {                                    
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	t.Execute(w, nil)
 }
